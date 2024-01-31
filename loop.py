7a22f20cfce573dad659aa1dbf6103a2eaf68975
Naim <mdnaim01303202218@gmail.com>
2024-01-31 19:43:15 +0600

    another queston solve

--- loop.py
+++ loop.py
@@ -16,7 +16,27 @@
 # *****
 
 # Pattern Printing
-n = 5  
-for i in range(n):
-    print('*' * n)
+# n = 5  
+# for i in range(n):
+#     print('*' * n)
 
+#question 3 print the given pattern 
+"""
+for n = 4 
+1234
+1234
+1234
+1234
+for n = 6 
+123456
+123456
+123456
+123456
+123456
+123456
+"""
+# n = int(input('give a number '))
+# for i in range(n):
+#     for j in range(1, n+1):
+#         print(j, end='')
+#     print()
