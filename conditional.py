8d4bd279df57740f5f3d11399ba733c463780f0a
Naim <mdnaim01303202218@gmail.com>
2024-01-31 13:39:45 +0600

    solve 3rd question

--- conditional.py
+++ conditional.py
@@ -25,17 +25,38 @@
 # c. 61-70 fair
 # d. <=40 fail
 
-number = int (input('how much you got the exam ? '))
+# number = int (input('how much you got the exam ? '))
 
-if number>=81:
-    print ('your grade is very good')
-elif number<81 and number>=71:
-    print('your result is good')
-elif number<=70 and number>=61:
-    print('your result is fair ')
-elif number<=61 and number>=40:
-    print('your result is poor')
-elif number<=40:
-    print('your result is fail')
+# if number>=81:
+#     print ('your grade is very good')
+# elif number<81 and number>=71:
+#     print('your result is good')
+# elif number<=70 and number>=61:
+#     print('your result is fair ')
+# elif number<=61 and number>=40:
+#     print('your result is poor')
+# elif number<=40:
+#     print('your result is fail')
+# else:
+#     print('invalid mark')
+
+
+
+
+#take a positive interger input and tell if it is a four digit number or not 
+
+a = (input('take a number '))
+
+length = len(a)
+
+if (length == 4):
+    print('this is 4 digit number')
+
 else:
-    print('invalid mark')
+    print('input size is ', length)
+
+a = int(input('Give me a number '))
+if (a>=1000 and a<=9999):
+    print('it is 4 digit number')
+else:
+    print('it is not 4 digit number')
