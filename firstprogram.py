71254e93caa722be25def20bb08fb7a3312ba304
Naim <mdnaim01303202218@gmail.com>
2024-01-31 13:04:33 +0600

    basic python

---
+++ firstprogram.py
@@ -0,0 +1,28 @@
+name = input('what is your name? ')
+print(" hello "+ name +" vai")
+
+superhero = input('what is your favorite superhero Name? ')
+
+supname = superhero
+
+print(supname)
+
+old_age = input('enter your old age : ')
+new_age = int(old_age) + 2
+
+print('your new age is ', new_age)
+
+first = input('enter your first number : ')
+second = input('enter your second number : ')
+
+print(first+ second)
+print(int(first)+int(second))
+
+name = "my name is naim sheikh"
+
+print(name.find('zx'))
+print(name.find('naim'))
+print(name.replace('naim sheikh','mim rahman'))
+
+print("naim" in name)
+print("mim" in name)
