eb6a53f0fefac04f5f8bf88541800688ebc01a57
Naim <mdnaim01303202218@gmail.com>
2024-01-31 19:35:29 +0600

    second question done in loop

--- loop.py
+++ loop.py
@@ -1,7 +1,22 @@
 # Loop Problem
 # Print elements of a list using for loop
 
-numbers = ['apple', 'banana', 'jackfruit', 4, 5]
+# numbers = ['apple', 'banana', 'jackfruit', 4, 5]
 
-for i in numbers:
-    print(i)
+# for i in numbers:
+#     print(i)
+
+
+# ques 2 Lets now solve pattern printing question 
+# print given pattern 
+# for n=1
+# *****
+# for n =2 
+# *****
+# *****
+
+# Pattern Printing
+n = 5  
+for i in range(n):
+    print('*' * n)
+
